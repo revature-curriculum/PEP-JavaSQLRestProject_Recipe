61925555c66387fb999315faccd7464db57792d2
Ashwin-revature <ashwin.ashok.c@revature.com>
2024-10-18 16:43:25 +0530

    Update IngredientController.java

--- reva-recipe/src/main/java/com/revature/controller/IngredientController.java
+++ reva-recipe/src/main/java/com/revature/controller/IngredientController.java
@@ -1,5 +1,73 @@
 package com.revature.controller;
 
+
+/**
+ * The IngredientController class handles operations related to ingredients.
+ * It allows for creating, retrieving, updating, and deleting individual ingredients,
+ * as well as retrieving a list of all ingredients. The class interacts with the 
+ * IngredientService to perform these operations.
+ */
+
 public class IngredientController {
+
+    /**
+     * A service that manages ingredient-related operations.
+     */
+
+    private IngredientService ingredientService;
+
+    /**
+     * Constructs an IngredientController with the specified IngredientService.
+     *
+     * @param ingredientService the service used to manage ingredient-related operations
+     */
+
+    public IngredientController(IngredientService ingredientService) {
+        this.ingredientService = ingredientService;
+    }
+
+    /**
+     * Handler for retrieving a single ingredient.
+     * 
+     * @return the ingredient details
+     */
+
+    public void getIngredient() {
+        // Implementation
+    }
+
+    /**
+     * Handler for deleting a specific ingredient.
+     */
+
+    public void deleteIngredient() {
+        // Implementation
+    }
+
+    /**
+     * Handler for updating a specific ingredient's details.
+     */
+
+    public void updateIngredient() {
+        // Implementation
+    }
+
+    /**
+     * Handler for creating a new ingredient.
+     */
+
+    public void createIngredient() {
+        // Implementation
+    }
+
+    /**
+     * Handler for retrieving a list of all ingredients.
+     * 
+     * @return the list of ingredients
+     */
     
+    public void getIngredients() {
+        // Implementation
+    }
 }
+
