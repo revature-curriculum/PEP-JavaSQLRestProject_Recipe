b8921d36cd5b1aa0fc2d2f0f317386318384bd66
Tanisi90 <69205416+Tanisi90@users.noreply.github.com>
2024-10-31 07:22:40 -0700

    Update ddl.sql
    Updated Table names and column name of table

--- reva-recipe/src/main/resources/ddl.sql
+++ reva-recipe/src/main/resources/ddl.sql
@@ -6,12 +6,12 @@
 -- 3. instructions: A non-nullable varchar field to store the recipe's instructions.
 -- 4. chef_id: A foreign key that references the 'id' field from the Chef table.
 
-CREATE TABLE Recipe (
+CREATE TABLE RECIPE (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) UNIQUE NOT NULL,
     instructions VARCHAR(255) NOT NULL,
     chef_id INT,
-    FOREIGN KEY (chef_id) REFERENCES Chef(id)
+    FOREIGN KEY (chef_id) REFERENCES CHEF(id)
 );
 
 -- Chef Table
@@ -23,12 +23,12 @@ CREATE TABLE Recipe (
 -- 4. password: A non-nullable varchar field to store the chef's password.
 -- 5. isAdmin: A boolean field to indicate if the chef has admin privileges.
 
-CREATE TABLE Chef (
+CREATE TABLE CHEF (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    isAdmin BOOLEAN
+    is_admin BOOLEAN
 );
 
 -- Ingredient Table
@@ -36,8 +36,7 @@ CREATE TABLE Chef (
 -- Fields:
 -- 1. id: An auto-incremented primary key to uniquely identify each ingredient.
 -- 2. name: A unique and non-nullable varchar field (max 20 characters) to store the ingredient's name.
-
-CREATE TABLE Ingredient (
+CREATE TABLE INGREDIENT (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(20) NOT NULL UNIQUE
 );
@@ -53,13 +52,78 @@ CREATE TABLE Ingredient (
 -- 5. unit: A non-nullable varchar field (max 20 characters) to store the unit of the volume.
 -- 6. is_metric: A boolean field to indicate if the unit is in metric. Defaults to false.
 
-CREATE TABLE Recipe_Ingredient (
+
+CREATE TABLE RECIPE_INGREDIENT (
     id INT AUTO_INCREMENT PRIMARY KEY,
     recipe_id INT NOT NULL,
     ingredient_id INT NOT NULL,
     vol DECIMAL(10,2) NOT NULL,
     unit VARCHAR(20) NOT NULL,
     is_metric BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (recipe_id) REFERENCES Recipe(id),
-    FOREIGN KEY (ingredient_id) REFERENCES Ingredient(id)
+    FOREIGN KEY (recipe_id) REFERENCES RECIPE(id),
+    FOREIGN KEY (ingredient_id) REFERENCES INGREDIENT(id)
 );
+
+DELETE FROM RECIPE_INGREDIENT;
+
+DELETE FROM RECIPE;
+
+DELETE FROM CHEF;
+
+DELETE FROM INGREDIENT;
+
+ALTER TABLE CHEF ALTER COLUMN id RESTART WITH 1;
+
+ALTER TABLE INGREDIENT ALTER COLUMN id RESTART WITH 1;
+
+ALTER TABLE RECIPE ALTER COLUMN id RESTART WITH 1;
+
+ALTER TABLE RECIPE_INGREDIENT ALTER COLUMN id RESTART WITH 1;
+
+INSERT INTO 
+CHEF 
+	(username, email, password, is_admin) 
+VALUES 
+	('JoeCool', 'snoopy@null.com', 'redbarron',false),
+	('CharlieBrown', 'goodgrief@peanuts.com', 'thegreatpumpkin', false),
+	('RevaBuddy', 'revature@revature.com', 'codelikeaboss', false),
+	('ChefTrevin', 'trevin@revature.com', 'trevature', true);
+
+INSERT INTO 
+RECIPE
+	(name, instructions, chef_id) 
+VALUES 
+	('carrot soup', 'Put carrot in water.  Boil.  Maybe salt.',1),
+	('potato soup', 'Put potato in water.  Boil.  Maybe salt.', 2),
+	('tomato soup', 'Put tomato in water.  Boil.  Maybe salt.', 2),
+	('lemon rice soup', 'Put lemon and rice in water.  Boil.  Maybe salt.', 4),
+	('stone soup', 'Put stone in water.  Boil.  Maybe salt.', 4);
+
+
+
+INSERT INTO
+INGREDIENT
+	(name)
+VALUES
+	('carrot'),
+	('potato'),
+	('tomato'),
+	('lemon'),
+	('rice'),
+	('stone');
+
+
+INSERT INTO
+RECIPE_INGREDIENT
+    (id, recipe_id, ingredient_id, vol, unit)
+VALUES
+    (default, 1, 1, 1, 'cups'),
+    (default, 2, 2, 2, 'cups'),
+    (default, 3, 3, 2, 'cups'),
+    (default, 4, 4, 1, 'Tbs'),
+    (default, 4, 5, 2, 'cups');
+
+
+
+
+
